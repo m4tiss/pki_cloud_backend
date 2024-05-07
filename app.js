6d69b1e490e491764a2b6b6ff60a872793c11e6b
m4tiss <mateusz039@op.pl>
2024-05-07 21:08:17 +0200

    clean code

--- app.js
+++ app.js
@@ -21,6 +21,70 @@ const GITHUB_CLIENT_ID = 'Ov23ligvIsDIXrZbv2Jl';
 const GITHUB_CLIENT_SECRET = '49ea434bd71dfcb02cec39cb4b8e17f6497f1b10';
 
 
+app.get('/', (req, res) => {
+    if (!authed) {
+        res.send('<a href="/login">Login with Google</a><a href="/loginGithub">Login with Github</a>');
+    } else {
+        var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' });
+        oauth2.userinfo.v2.me.get(function (err, result) {
+            if (err) {
+                console.log('Niestety bład!');
+                console.log(err);
+            } else {
+                loggedUser = result.data.name;
+                console.log(loggedUser);
+            }
+            res.send('Logged in: '.concat(loggedUser, '<img src="', result.data.picture, '"height="23" width="23"> <br><a href="/logout">Logout</a>'));
+        });
+    }
+});
+
+app.get('/login', (req, res) => {
+    const url = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile'
+    });
+    res.redirect(url);
+});
+
+app.get('/auth/google/callback', function (req, res) {
+    const code = req.query.code;
+    if (code) {
+        oAuth2Client.getToken(code, function (err, tokens) {
+            if (err) {
+                console.log('Error authenticating');
+                console.log(err);
+            } else {
+                console.log('Successfully authenticated');
+                oAuth2Client.setCredentials(tokens);
+                authed = true;
+                res.redirect('/');
+            }
+        });
+    }
+});
+
+app.get('/logout', (req, res) => {
+    if (authed) {
+        oAuth2Client.revokeCredentials(function(err, result) {
+            if (err) {
+                console.error('Error revoking credentials:', err);
+            } else {
+                console.log('Credentials successfully revoked');
+            }
+            authed = false;
+            res.redirect('/');
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/loginGithub', (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&prompt=consent`);
+});
+
+
 app.get('/github/callback', (req, res) => {
     const requestToken = req.query.code;
 
@@ -53,74 +117,12 @@ app.get('/github/callback', (req, res) => {
     });
 });
 
-
-app.get('/', (req, res) => {
-    if (!authed) {
-        res.send('<a href="/login">Login with Google</a><a href="/loginGithub">Login with Github</a>');
-    } else {
-        var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' });
-        oauth2.userinfo.v2.me.get(function (err, result) {
-            if (err) {
-                console.log('Niestety bład!');
-                console.log(err);
-            } else {
-                loggedUser = result.data.name;
-                console.log(loggedUser);
-            }
-            res.send('Logged in: '.concat(loggedUser, '<img src="', result.data.picture, '"height="23" width="23"> <br><a href="/logout">Logout</a>'));
-        });
-    }
-});
-
- app.get('/loginGithub', (req, res) => {
-     res.redirect(`https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&prompt=consent`);
- });
-
-app.get('/login', (req, res) => {
-    const url = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: 'https://www.googleapis.com/auth/userinfo.profile'
-    });
-    res.redirect(url);
-});
-
-app.get('/logout', (req, res) => {
-    if (authed) {
-        oAuth2Client.revokeCredentials(function(err, result) {
-            if (err) {
-                console.error('Error revoking credentials:', err);
-            } else {
-                console.log('Credentials successfully revoked');
-            }
-            authed = false;
-            res.redirect('/');
-        });
-    } else {
-        res.redirect('/');
-    }
-});
-
  app.get("/logoutGithub", function (req, res) {
      access_token = ""
      res.redirect('/');
    })
 
-app.get('/auth/google/callback', function (req, res) {
-    const code = req.query.code;
-    if (code) {
-        oAuth2Client.getToken(code, function (err, tokens) {
-            if (err) {
-                console.log('Error authenticating');
-                console.log(err);
-            } else {
-                console.log('Successfully authenticated');
-                oAuth2Client.setCredentials(tokens);
-                authed = true;
-                res.redirect('/');
-            }
-        });
-    }
-});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running at ${port}`));
