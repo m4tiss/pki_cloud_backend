4c5c9b35efa653be61a48807e8ef15de235bbb76
m4tiss <mateusz039@op.pl>
2024-05-14 15:35:56 +0200

    change alert

--- app.js
+++ app.js
@@ -166,10 +166,26 @@ app.get('/', (req, res) => {
                         </div>
                       </div>
                     </div>
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    Połączenie z bazą danych zostało ustanowione.
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#successModal">
+                    Show Success Modal
+                  </button>
+                  
+                  <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="successModalLabel">Success Modal</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          Połączenie z bazą danych zostało ustanowione.
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                      </div>
                     </div>
+                  </div>
                     `;
                     
 
@@ -326,10 +342,27 @@ app.get('/github/callback', (req, res) => {
                         </div>
                       </div>
                     </div>
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        Połączenie z bazą danych zostało ustanowione.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                     </div>
+
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#successModal">
+                    Show Success Modal
+                  </button>
+                  
+                  <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="successModalLabel">Success Modal</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          Połączenie z bazą danych zostało ustanowione.
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                     `;
                 res.send(htmlContent);
             }
