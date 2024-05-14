f2923b189e5db0c7b276347477db9959008edf0e
m4tiss <mateusz039@op.pl>
2024-05-14 15:42:10 +0200

    test toast

--- app.js
+++ app.js
@@ -166,23 +166,15 @@ app.get('/', (req, res) => {
                         </div>
                       </div>
                     </div>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#successModal">
-                    Show Success Modal
-                  </button>
-                  
-                  <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="successModalLabel">Success Modal</h5>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                          Połączenie z bazą danych zostało ustanowione.
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
+                    
+                    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 5">
+                    <div id="successToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                      <div class="toast-header">
+                        <strong class="me-auto">Success</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                      </div>
+                      <div class="toast-body">
+                        Połączenie z bazą danych zostało ustanowione.
                       </div>
                     </div>
                   </div>
